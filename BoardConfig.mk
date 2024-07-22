53db5a35eef14d799d1bf3bd8f328aac6b4a8b8d
Luca Stefani <luca.stefani.ge1@gmail.com>
2024-07-22 10:47:48 +0200

    seur: Enable updateable APEX

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,6 +7,9 @@
 DEVICE_PATH := device/xiaomi/seur
 PREBUILT_COMPONENTS_PATH := device/xiaomi/seur-prebuilt
 
+# APEX
+DEXPREOPT_GENERATE_APEX_IMAGE := true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a-dotprod
