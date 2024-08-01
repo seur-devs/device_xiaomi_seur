b399fd1af207f0435c0fe6d79bfae5c21a2febbd
bengris32 <bengris32@protonmail.ch>
2024-08-02 01:38:28 +0200

    seur: Build `android.hidl.allocator@1.0` vendor variant
    * Dependency of PQ HAL.

--- device.mk
+++ device.mk
@@ -135,7 +135,8 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     libhidltransport.vendor \
-    libhwbinder.vendor
+    libhwbinder.vendor \
+    android.hidl.allocator@1.0.vendor
 
 # IMS
 PRODUCT_BOOT_JARS += \
