a383bb316fc2617ac424f1d6fa0fd1ace707d9c2
Matsvei Niaverau <matvej2002@gmail.com>
2024-08-01 22:19:28 +0200

    seur: Configure Thermal Stack

--- device.mk
+++ device.mk
@@ -193,6 +193,13 @@ PRODUCT_SOONG_NAMESPACES := \
     hardware/xiaomi \
     hardware/mediatek
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl
+
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0.vendor
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-mediatekv2
